df650864ad10605b5a5cd7e2fb398569caa571c9
mrnelke <120826750+mrnelke@users.noreply.github.com>
2023-04-11 00:28:50 +0300

    changed stylesheet to template directory

--- function.php
+++ function.php
@@ -5,7 +5,7 @@ function your_theme_enqueue_styles() {
 	$parent_style = 'hivetheme-parent-frontend';
 
 	wp_enqueue_style( 'child-style',
-		get_stylesheet_directory_uri() . '/style.css',
+		get_template_directory_uri() . '/style.css',
 		array($parent_style),
 		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
 	);
